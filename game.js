f6bb69d88d2dbaf8a6a01a80ae6936565f1c80fd
Stefan Girlich <stefan.girlich@gmail.com>
2017-06-09 16:43:45 +0200

    Move game logic to own class

---
+++ game.js
@@ -0,0 +1,94 @@
+const createInitialDoorStates = (count) => {
+	const result = []
+	for (let i = 0; i < count; i++) {
+		result.push(false)
+	}
+
+	return result
+}
+
+
+
+
+const State = {
+    STATE_START: 0,
+    STATE_QUESTION: 1,
+    STATE_END_WON: 2,
+    STATE_END_LOST: 3,
+}
+
+class Game {
+
+	constructor(doorCount, changeListener) {
+		this.doorCount = doorCount
+		this.reset()
+		this.changeListener = changeListener
+	}
+
+	selectInitialDoor(doorPos) {
+		if (this.state !== State.STATE_START) {
+			throw new Error('cannot select initial door anymore')
+		}
+
+		if (doorPos < 0 || doorPos > this.doorStates.length - 1) {
+			throw new Error('door position needs to be in range [0;doorCount[')
+		}
+
+		const self = this
+
+		this.playerPos = doorPos
+		this.state = State.STATE_QUESTION
+
+		const goatDoorIndices = this.doorStates
+			.map((value, index) => index)
+			.filter((index) => index !== self.playerPos && index !== self.carPos)
+
+		const hasPlayedSelectedCar = self.playerPos === self.carPos
+		const randomGoatDoorIndicesIndex = Math.floor(Math.random() * goatDoorIndices.length)
+		const randomGoatToHidePos = hasPlayedSelectedCar ? goatDoorIndices[randomGoatDoorIndicesIndex] : null
+
+		goatDoorIndices.forEach((goatDoorIndex) => {
+			this.doorStates[goatDoorIndex] = goatDoorIndex !== randomGoatToHidePos
+		})
+
+		const freeDoorIndex = this.doorStates
+			.map((value, index) => index)
+			.filter((index) => index !== self.playerPos && !self.doorStates[index])[0]
+
+		this.switchPos = freeDoorIndex
+
+		this.changeListener()
+
+		return freeDoorIndex
+	}
+
+	selectSwitchOrNot(shouldSwitch) {
+		if (this.state !== State.STATE_QUESTION) {
+			throw new Error('cannot select switch right now')
+		}
+
+		if (shouldSwitch) {
+			this.playerPos = this.switchPos
+		}
+
+		this.doorStates.forEach((isOpen, index) => this.doorStates[index] = true)
+
+		const won = this.playerPos === this.carPos
+
+		this.state = won ? State.STATE_END_WON : State.STATE_END_LOST
+		this.changeListener()
+	}
+
+	reset() {
+		this.state = State.STATE_START
+		this.doorStates = createInitialDoorStates(this.doorCount)
+		this.carPos = Math.floor(Math.random() * this.doorStates.length)
+		this.playerPos = null
+		this.switchPos = null
+
+		//console.log('>>>>>>>>>>>> DEBUG: car is in ' + this.carPos + '  ' + getDoorNameFromIndex(this.carPos))
+	}
+}
+
+module.exports.Game = Game
+module.exports.State = State
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const readlineSync = require('readline-sync')
 const config = require('./config.json')
+const Game = require('./game')
 
 const formatText = (template, params) => {
 	params = params.forEach ? params : [params]
@@ -18,14 +19,7 @@ const createDoorNameMap = (count) => {
 	return doorNamesToIndices
 }
 
-const createInitialDoorStates = (count) => {
-	const result = []
-	for (let i = 0; i < count; i++) {
-		result.push(false)
-	}
 
-	return result
-}
 
 const getDoorIndexFromName = (name) => {
 	if (!(name in doorNamesToIndices)) {
@@ -43,83 +37,7 @@ const getDoorNameFromIndex = (index) => {
 	return singleDoorName[0]
 }
 
-const STATE_START = 0
-const STATE_QUESTION = 1
-const STATE_END_WON = 2
-const STATE_END_LOST = 3
 
-class Game {
-
-	constructor(doorCount, changeListener) {
-		this.doorCount = doorCount
-		this.reset()
-		this.changeListener = changeListener
-	}
-
-	selectInitialDoor(doorPos) {
-		if (this.state !== STATE_START) {
-			throw new Error('cannot select initial door anymore')
-		}
-
-		if (doorPos < 0 || doorPos > this.doorStates.length - 1) {
-			throw new Error('door position needs to be in range [0;doorCount[')
-		}
-
-		const self = this
-
-		this.playerPos = doorPos
-		this.state = STATE_QUESTION
-
-		const goatDoorIndices = this.doorStates
-			.map((value, index) => index)
-			.filter((index) => index !== self.playerPos && index !== self.carPos)
-
-		const hasPlayedSelectedCar = self.playerPos === self.carPos
-		const randomGoatDoorIndicesIndex = Math.floor(Math.random() * goatDoorIndices.length)
-		const randomGoatToHidePos = hasPlayedSelectedCar ? goatDoorIndices[randomGoatDoorIndicesIndex] : null
-
-		goatDoorIndices.forEach((goatDoorIndex) => {
-			this.doorStates[goatDoorIndex] = goatDoorIndex !== randomGoatToHidePos
-		})
-
-		const freeDoorIndex = this.doorStates
-			.map((value, index) => index)
-			.filter((index) => index !== self.playerPos && !self.doorStates[index])[0]
-
-		this.switchPos = freeDoorIndex
-
-		this.changeListener()
-
-		return freeDoorIndex
-	}
-
-	selectSwitchOrNot(shouldSwitch) {
-		if (this.state !== STATE_QUESTION) {
-			throw new Error('cannot select switch right now')
-		}
-
-		if (shouldSwitch) {
-			this.playerPos = this.switchPos
-		}
-
-		this.doorStates.forEach((isOpen, index) => this.doorStates[index] = true)
-
-		const won = this.playerPos === this.carPos
-
-		this.state = won ? STATE_END_WON : STATE_END_LOST
-		this.changeListener()
-	}
-
-	reset() {
-		this.state = STATE_START
-		this.doorStates = createInitialDoorStates(this.doorCount)
-		this.carPos = Math.floor(Math.random() * this.doorStates.length)
-		this.playerPos = null
-		this.switchPos = null
-
-		//console.log('>>>>>>>>>>>> DEBUG: car is in ' + this.carPos + '  ' + getDoorNameFromIndex(this.carPos))
-	}
-}
 
 
 
@@ -154,9 +72,9 @@ const render = () => {
 	console.log(output)
 	console.log('\n')
 
-	if (game.state === STATE_END_WON) {
+	if (game.state === Game.State.STATE_END_WON) {
 		console.log(config.texts.wonMessage)
-	} else if (game.state === STATE_END_LOST) {
+	} else if (game.state === Game.State.STATE_END_LOST) {
 		console.log(config.texts.lostMessage)
 	}
 
@@ -182,7 +100,7 @@ if (doorCount < 3) {
 }
 
 const doorNamesToIndices = createDoorNameMap(doorCount)
-const game = new Game(doorCount, render)
+const game = new Game.Game(doorCount, render)
 
 
 const results = {
@@ -225,7 +143,7 @@ while (true) {
 	game.selectSwitchOrNot(shouldSwitch)
 
 	let resultKey;
-	if (game.state === STATE_END_WON) {
+	if (game.state === Game.State.STATE_END_WON) {
 		resultKey = shouldSwitch ? 'wonBySwitching' : 'wonByStaying'
 	} else {
 		resultKey = shouldSwitch ? 'lostBySwitching' : 'lostByStaying'
